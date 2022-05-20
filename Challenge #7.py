79071a37c05f80f61ae1fcb3b8547488c28a6806
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-05-20 19:55:12 -0400

    Update Challenge #7.py

--- Challenge #7.py
+++ Challenge #7.py
@@ -121,3 +121,6 @@ class Graph:
                 return None
                          
         return self.printArr(dist, src)
+      
+      
+solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1)
